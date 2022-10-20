eae79adf703e7d35c8e9d2a94641cbcb7921e030
zdelo01 <115585985+zdelo01@users.noreply.github.com>
2022-10-20 16:51:18 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -25,22 +25,21 @@ Return:
 */
 void CallProcedure(string pName)
 {
-	char* procname = new char[pName.length() + 1];
-	std::strcpy(procname, pName.c_str());
+    char* procname = new char[pName.length() + 1];
+    std::strcpy(procname, pName.c_str());
 
-	Py_Initialize();
-	PyObject* my_module = PyImport_ImportModule("PythonCode");
-	PyErr_Print();
-	PyObject* my_function = PyObject_GetAttrString(my_module, procname);
-	PyObject* my_result = PyObject_CallObject(my_function, NULL);
-	Py_Finalize();
+    Py_Initialize();
+    PyObject* my_module = PyImport_ImportModule("PythonCode");
+    PyErr_Print();
+    PyObject* my_function = PyObject_GetAttrString(my_module, procname);
+    PyObject* my_result = PyObject_CallObject(my_function, NULL);
+    Py_Finalize();
 
-	delete[] procname;
+    delete[] procname;
 }
 
 /*
-Description:
-	To call this function, pass the name of the Python functino you wish to call and the string parameter you want to send
+Description: To call this function, pass the name of the Python functino you wish to call and the string parameter you want to send
 Example:
 	int x = callIntFunc("PrintMe","Test");
 Output:
@@ -71,14 +70,14 @@ int callIntFunc(string proc, string param)
 	pFunc = PyDict_GetItemString(pDict, procname);
 	if (PyCallable_Check(pFunc))
 	{
-		pValue = Py_BuildValue("(z)", paramval);
-		PyErr_Print();
-		presult = PyObject_CallObject(pFunc, pValue);
-		PyErr_Print();
+	    pValue = Py_BuildValue("(z)", paramval);
+	    PyErr_Print();
+	    presult = PyObject_CallObject(pFunc, pValue);
+	    PyErr_Print();
 	}
 	else
 	{
-		PyErr_Print();
+	    PyErr_Print();
 	}
 	//printf("Result is %d\n", _PyLong_AsInt(presult));
 	Py_DECREF(pValue);
@@ -106,43 +105,43 @@ Return:
 */
 int callIntFunc(string proc, int param)
 {
-	char* procname = new char[proc.length() + 1];
-	std::strcpy(procname, proc.c_str());
+    char* procname = new char[proc.length() + 1];
+    std::strcpy(procname, proc.c_str());
 
-	PyObject* pName, * pModule, * pDict, * pFunc, * pValue = nullptr, * presult = nullptr;
-	// Initialize the Python Interpreter
-	Py_Initialize();
-	// Build the name object
-	pName = PyUnicode_FromString((char*)"PythonCode");
-	// Load the module object
-	pModule = PyImport_Import(pName);
-	// pDict is a borrowed reference 
-	pDict = PyModule_GetDict(pModule);
-	// pFunc is also a borrowed reference 
-	pFunc = PyDict_GetItemString(pDict, procname);
-	if (PyCallable_Check(pFunc))
-	{
-		pValue = Py_BuildValue("(i)", param);
-		PyErr_Print();
-		presult = PyObject_CallObject(pFunc, pValue);
-		PyErr_Print();
-	}
-	else
-	{
-		PyErr_Print();
-	}
-	//printf("Result is %d\n", _PyLong_AsInt(presult));
-	Py_DECREF(pValue);
-	// Clean up
-	Py_DECREF(pModule);
-	Py_DECREF(pName);
-	// Finish the Python Interpreter
-	Py_Finalize();
+    PyObject* pName, * pModule, * pDict, * pFunc, * pValue = nullptr, * presult = nullptr;
+    // Initialize the Python Interpreter
+    Py_Initialize();
+    // Build the name object
+    pName = PyUnicode_FromString((char*)"PythonCode");
+    // Load the module object
+    pModule = PyImport_Import(pName);
+    // pDict is a borrowed reference 
+    pDict = PyModule_GetDict(pModule);
+    // pFunc is also a borrowed reference 
+    pFunc = PyDict_GetItemString(pDict, procname);
+    if (PyCallable_Check(pFunc))
+    {
+	pValue = Py_BuildValue("(i)", param);
+	PyErr_Print();
+	presult = PyObject_CallObject(pFunc, pValue);
+	PyErr_Print();
+    }
+    else
+    {
+	PyErr_Print();
+    }
+    //printf("Result is %d\n", _PyLong_AsInt(presult));
+    Py_DECREF(pValue);
+    // Clean up
+    Py_DECREF(pModule);
+    Py_DECREF(pName);
+    // Finish the Python Interpreter
+    Py_Finalize();
 
-	// clean 
-	delete[] procname;
+    // clean 
+    delete[] procname;
 
-	return _PyLong_AsInt(presult);
+    return _PyLong_AsInt(presult);
 }
 
 /*
